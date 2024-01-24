0a5a5b15a9003d8ef6feb8855b309cbb10aa61ac
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-24 16:21:25 +0530

    Update nstart.py

--- YukkiMusic/plugins/bot/nstart.py
+++ YukkiMusic/plugins/bot/nstart.py
@@ -66,8 +66,9 @@ async def start(client, message: Message, _):
         random.choice(YUMI_PICS),
         caption=_["start_2"].format(config.MUSIC_BOT_NAME),
         reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("★ Add Me ★", url="https://t.me/I_Love_You_828")]
+          [
+                InlineKeyboardButton(
+                  "★ Add Me ★", url="https://t.me/I_Love_You_828")
             ]
           [
             InlineKeyboardButton(
