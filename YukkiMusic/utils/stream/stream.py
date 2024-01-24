e93f52d20e157a85af4ada990ce8270c327a1d24
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-24 22:37:29 +0530

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -28,8 +28,7 @@ from YukkiMusic.utils.inline.play import (stream_markup, queue_markup,
 from YukkiMusic.utils.inline.playlist import close_markup
 from YukkiMusic.utils.pastebin import Yukkibin
 from YukkiMusic.utils.stream.queue import put_queue, put_queue_index
-from YukkiMusic.utils.thumbnails import (gen_thumb, 
-                                         gen_qthumb‎)
+from YukkiMusic.utils.thumbnails import gen_thumb, gen_qthumb‎
 
 
 async def stream(
