c58d6ba61c66958a6c639cea28a8c8d8b73d20d2
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-24 21:20:19 +0530

    Update call.py

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -44,7 +44,7 @@ from YukkiMusic.utils.exceptions import AssistantErr
 from YukkiMusic.utils.inline.play import (stream_markup,
                                           telegram_markup)
 from YukkiMusic.utils.stream.autoclear import auto_clean
-from YukkiMusic.utils.thumbnails import gen_thumb, gen_qthumb
+from YukkiMusic.utils.thumbnails import gen_thumb
 
 autoend = {}
 counter = {}
