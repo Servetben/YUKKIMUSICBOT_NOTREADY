752a3150779e76cbefcbde19af1438e77aa69a09
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-26 20:19:24 +0530

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -89,7 +89,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb10":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "Only for Sudo Users", show_alert=True
+                "ʜᴇʏʏ ɪᴛ's ɴᴏᴛ ғᴏʀ ʏᴏᴜ ᴘʟᴇᴀsᴇ ᴅᴏɴ'ᴛ ᴛᴏᴜᴄʜ ɪᴛ ᴀɢᴀɪɴ", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
