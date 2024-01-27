5f458ebc3a2ec38a0cd597958582a849661c4adf
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-27 19:36:11 +0530

    Update nstart.py

--- YukkiMusic/utils/inline/nstart.py
+++ YukkiMusic/utils/inline/nstart.py
@@ -1,1 +1,29 @@
+from typing import Union
 
+from pyrogram.types import InlineKeyboardButton
+
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from YukkiMusic import app
+
+def nstart_pannel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+        ],
+        [
+            InlineKeyboardButton("• ʙᴏᴛ ɪɴғᴏ •", callback_data="bot_info_data"),
+        ],
+    ]
+    return buttons
