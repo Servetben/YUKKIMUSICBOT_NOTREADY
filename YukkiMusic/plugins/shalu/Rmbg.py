b70f34fd12483c9f4e80547396f76e7a2be1b6cd
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-27 15:01:12 +0530

    Update Rmbg.py

--- YukkiMusic/plugins/shalu/Rmbg.py
+++ YukkiMusic/plugins/shalu/Rmbg.py
@@ -34,7 +34,7 @@ async def RemoveBG(input_file_name):
             return True, name
 
 
-@app.on_message(filters.command(["rmbg", "bgremove"))
+@app.on_message(filters.command(["rmbg", "bgremove"]))
 async def rmbg(bot, message):
   rmbg = await message.reply("Processing...") 
   replied = message.reply_to_message
