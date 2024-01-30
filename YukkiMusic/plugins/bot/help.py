d08fa82aa64c9649b43d9c2ecf7d557cde8dc2de
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-30 17:05:17 +0530

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -128,4 +128,18 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
     elif cb == "hb15":
         await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
+    elif cb == "hb20":
+        await CallbackQuery.edit_message_text(helpers.HELP_20, reply_markup=keyboard)
 
+@app.on_callback_query(filters.regex("next_inline"))
+async def next_innline(_, callback_query):
+   await callback_query.edit_message_media(
+     reply_markup=InlineKeyboardMarkup(
+            [
+                InlineKeyboardButton(
+                  text=_["H_B_20_"],
+                  callback_data="help_callback hb20",
+                ),
+            ]
+        ),
+   )
