7504a017e92cc63674b58f103d147883b1ff92c2
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-31 15:22:08 +0530

    Update couples.py

--- YukkiMusic/plugins/tools/couples.py
+++ YukkiMusic/plugins/tools/couples.py
@@ -73,8 +73,8 @@ async def ctest(_, message):
 
          img = Image.open("Love/NewCouple.jpg")
 
-         img1 = img1.resize((760,760))
-         img2 = img2.resize((760,760))
+         img1 = img1.resize((745,745))
+         img2 = img2.resize((745,745))
 
          mask = Image.new('L', img1.size, 0)
          draw = ImageDraw.Draw(mask) 
@@ -91,7 +91,7 @@ async def ctest(_, message):
          draw = ImageDraw.Draw(img)
 
          img.paste(img1, (250, 2540), img1)
-         img.paste(img2, (1380, 2540), img2)
+         img.paste(img2, (1398, 2540), img2)
 
          img.save(f'test_{cid}.png')
     
