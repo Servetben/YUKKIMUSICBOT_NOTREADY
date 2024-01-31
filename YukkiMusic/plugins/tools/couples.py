4c0a93030a79b53b174b2730545012f213fb7f5a
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-01-31 14:08:24 +0530

    Update couples.py

--- YukkiMusic/plugins/tools/couples.py
+++ YukkiMusic/plugins/tools/couples.py
@@ -90,7 +90,7 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (900, 1000), img1)
+         img.paste(img1, (900, 1500), img1)
          img.paste(img2, (2875, 1415), img2)
 
          img.save(f'test_{cid}.png')
