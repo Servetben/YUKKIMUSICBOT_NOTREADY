5289393124bd22d80ea1929a998033553ac7cf05
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-01 14:28:13 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -378,7 +378,7 @@ async def start_command(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                  if message.chat.type == ChatType.PRIVATE:
+        if message.chat.type == ChatType.PRIVATE:
         accha = await messsage.reply_text(
             text=random.choice(EMOJIOS),
         )
