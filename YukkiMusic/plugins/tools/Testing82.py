2dc58786d13bde0156eee6f5c0033e2e2d052661
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-01 16:24:55 +0530

    Update Testing82.py

--- YukkiMusic/plugins/tools/Testing82.py
+++ YukkiMusic/plugins/tools/Testing82.py
@@ -4,14 +4,15 @@ from YukkiMusic import app
 from pyrogram.types import InputMediaPhoto
 
 
-@app.on_message(filters.command("tele"))
-async def telegraphs(client, message):
+@app.on_message(filters.command(["tele"]))
+def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("Please Wait!")
+        i = message.reply("Please wait..")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
             url = "https://graph.org" + x
-            
-        i.edit(f'``{url}``')
+
+        i.edit(f'{url}')
+      
