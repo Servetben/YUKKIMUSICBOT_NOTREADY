0faa57f8bc6c28484ae30bd396caa4935c040327
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-01 16:04:31 +0530

    Create Testing82.py

---
+++ YukkiMusic/plugins/tools/Testing82.py
@@ -0,0 +1,17 @@
+from telegraph import upload_file
+from pyrogram import filters
+from YukkiMusic import app
+from pyrogram.types import InputMediaPhoto
+
+
+@app.on_message(filters.command("tl"))
+async def telegraphs(client, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("Please Wait!")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://graph.org" + x
+            
+        i.edit(f'``{url}``')
