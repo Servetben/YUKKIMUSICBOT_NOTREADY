ae46023a8189c846525b5be58f8e4745c8fb0089
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-02 15:34:07 +0530

    Update callback.py

--- YukkiMusic/plugins/admins/callback.py
+++ YukkiMusic/plugins/admins/callback.py
@@ -381,7 +381,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 run = await CallbackQuery.message.reply_photo(
                     photo=img,
                     caption=_["stream_1"].format(
-                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        f"https://t.me/{app.username}?start=info_{videoid}",
                         title[:23],
                         duration_min,
                         user_name,
