94a31e956a877afcddb2de5423818a79a2bc6627
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-10 22:13:18 +0530

    Update Left.py

--- YukkiMusic/plugins/tools/Left.py
+++ YukkiMusic/plugins/tools/Left.py
@@ -33,8 +33,8 @@ async def get_userinfo_img(
 
         circular_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
         circular_img.paste(img, (0, 0), mask)
-        resized = circular_img.resize((430, 430))
-        bg.paste(resized, (850, 220), resized)
+        resized = circular_img.resize((400, 400))
+        bg.paste(resized, (820, 190), resized)
 
     img_draw = ImageDraw.Draw(bg)
 
