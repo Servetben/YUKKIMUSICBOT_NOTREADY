5eae085a02407bff1f81b780db2fe227b80e47f5
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-15 15:36:15 +0530

    Update Randomphoto.py

--- YukkiMusic/plugins/tools/Randomphoto.py
+++ YukkiMusic/plugins/tools/Randomphoto.py
@@ -10,9 +10,38 @@ regex_photo = ["waifu", "neko"]
 pht = random.choice(regex_photo)
 url = f"https://api.waifu.pics/sfw/{pht}"
 
-@app.on_callback_query()
+
+@app.on_message(
+    filters.command("ZAnimeph") & filters.private)
+def get_waifu(client, message):
+    response = requests.get(url).json()
+    up = response['url']
+    if up:
+        but = [
+            [
+                InlineKeyboardButton(
+                    "summon me ",
+                    url=f"https://t.me/meee",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    "Generate again ✨", 
+                    callback_data="again",
+                ),
+                InlineKeyboardButton(
+                    "Owner",
+                    url=f"https://t.me/kwajan",
+                ),
+            ],
+        ]
+        markup = InlineKeyboardMarkup(but)
+        message.reply_photo(up, caption="**animeGenBot**", reply_markup=markup)
+    else:
+        message.reply("Request failed try /again")
+                
+@app.on_callback_query(filters.regex("again"))
 async def handle_query(client, query):
-    if query.data == "again":
         response = requests.get(url).json()
         up = response['url']
         if up:
@@ -38,34 +67,4 @@ async def handle_query(client, query):
             await query.message.reply_photo(up, caption="**animeGenBot**", reply_markup=markup)
         else:
             await query.message.reply("Request failed try /again")
-
-@app.on_message(
-    filters.command("Animezz", prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
-    & filters.private & filters.group)
-def get_waifu(client, message):
-    response = requests.get(url).json()
-    up = response['url']
-    if up:
-        but = [
-            [
-                InlineKeyboardButton(
-                    "summon me ",
-                    url=f"https://t.me/meee",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    "Generate again ✨", 
-                    callback_data=f'again',
-                ),
-                InlineKeyboardButton(
-                    "Owner",
-                    url=f"https://t.me/kwajan",
-                ),
-            ],
-        ]
-        markup = InlineKeyboardMarkup(but)
-        message.reply_photo(up, caption="**animeGenBot**", reply_markup=markup)
-    else:
-        message.reply("Request failed try /again")
-                
+    
