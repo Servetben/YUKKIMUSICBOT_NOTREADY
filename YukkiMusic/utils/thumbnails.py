eb5c57c87f19c764764d6942bed231385c323e37
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-25 15:08:20 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -84,10 +84,10 @@ async def gen_thumb(videoid, user_id):
         f = Image.fromarray(e)
         x = f.resize((107, 107))
 
-        images = random.choice(thumbs)
+        
         border = random.choice(colors)
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"YukkiMusic/Lol/{images}.png")
+        bg = Image.open(f"YukkiMusic/Lol/thumb_06.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
