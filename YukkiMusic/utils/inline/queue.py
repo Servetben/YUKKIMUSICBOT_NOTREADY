11d5bd1a6862f5db28338ba38ce59bebddc883e9
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-25 20:23:31 +0530

    Update queue.py

--- YukkiMusic/utils/inline/queue.py
+++ YukkiMusic/utils/inline/queue.py
@@ -57,19 +57,14 @@ def queue_markup(
     return upl
 
 
-def queue_back_markup(_, CPLAY):
-    upl = InlineKeyboardMarkup(
+def aq_markup(_, chat_id):
+    buttons = [
         [
-            [
-                InlineKeyboardButton(
-                    text=_["BACK_BUTTON"],
-                    callback_data=f"queue_back_timer {CPLAY}",
-                ),
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close",
-                ),
-            ]
-        ]
-    )
-    return upl
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
+    return buttons
