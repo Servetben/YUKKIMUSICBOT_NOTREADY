d8b0ae0e7978ae2b0025076def6059452c994ea9
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-26 12:57:39 +0530

    Update Id.py

--- YukkiMusic/plugins/tools/Id.py
+++ YukkiMusic/plugins/tools/Id.py
@@ -40,8 +40,8 @@ async def get_userinfo_img(
 
         circular_img = Image.new("RGBA", img.size, (0, 0, 0, 0))
         circular_img.paste(img, (0, 0), mask)
-        resized = circular_img.resize((400, 400))
-        bg.paste(resized, (500, 90), resized)
+        resized = circular_img.resize((500, 500))
+        bg.paste(resized, (800, 120), resized)
         
     img_draw = ImageDraw.Draw(bg)
     img_draw.text(
